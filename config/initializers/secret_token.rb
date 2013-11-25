8235d482be31e9854f083e14d8bf916e10d30628
q45 <quintinsmith@icloud.com>
2013-11-25 11:54:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emco::Application.config.secret_key_base = 'b4d3315a92d04ddb4c76f58fbf60279f42a8d8e1e2ec92cf069820fdc32f5d641f365fc14a7f750bfc97a7a3cd2a68e5cc41c30d9b9b7ad5c20224f8d9b2fc08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emco::Application.config.session_store :cookie_store, key: '_emco_session'
